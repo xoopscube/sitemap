2d663b9447ec27f0f94b90dda233e93fd677137f
Gigamaster <1905497+gigamaster@users.noreply.github.com>
2021-04-22 02:38:17 +0200

    :fire: Removing files unused

--- language/catalan/blocks.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('_MB_SITEMAP_HOME','HOME');
-
-define('_MB_SITEMAP_OPENCLOSE', 'Fes clic aqu� per mostrar/amagar');
-define('_MB_SITEMAP_COLS', 'Columnes');
--- language/french/blocks.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('_MB_SITEMAP_HOME','Accueil');
-
-define('_MB_SITEMAP_OPENCLOSE', 'Click!!(Ouvrir/Fermer)');
-define('_MB_SITEMAP_COLS', 'Colonnes');
-
